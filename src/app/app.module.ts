469e514d13dcdfc5a842b04331181cb2677579c7
Ahmed <ghoul.ahmed5@gmail.com>
2017-10-29 16:21:58 +0100

    finished example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { ButtonsComponent } from './counter/buttons/buttons.component';
+import { IAppStore, rootReducer, INIT_STATE } from './store'
 
+import { NgReduxModule, NgRedux, DevToolsExtension } from 'ng2-redux'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    ButtonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+  constructor(private ngRedux: NgRedux<IAppStore>, private dev: DevToolsExtension) {
+    let enhancer = (isDevMode) ? [dev.enhancer()] : [];
+    this.ngRedux.configureStore(rootReducer, INIT_STATE, [], enhancer)
+
+  }
+
+}
